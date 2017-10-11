5d7dc41e6444d0e5772a80cc0df9352a9324dd64
CT Shen <32235978+morris4030@users.noreply.github.com>
2017-10-11 14:45:07 +0800

    Update our_environment.R

--- our_environment.R
+++ our_environment.R
@@ -1,5 +1,8 @@
 list(
 morris = x86_64-w64-mingw32/x64 (64-bit)
-
-
+tsai = x86_64-w64-mingw32/x64 (64-bit)
+chen = x86_64-w64-mingw32/x64 (64-bit)
+alison = x86_64-w64-mingw32/x64 (64-bit)
+  
+  
 )
